614d5b97a416bc1b337bffcc6883bd50b6aec631
Ajay <ajayidc007@gmail.com>
2020-02-24 15:33:18 +0530

    Update CharacterPictureGrid.py
    modified by Aj

--- chapter 04/CharacterPictureGrid.py
+++ chapter 04/CharacterPictureGrid.py
@@ -3,29 +3,29 @@
 
 # Say you have a list of lists where each value in the inner lists is a one-character string, like this:
 
-grid = [['.', '.', '.', '.', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['.', 'O', 'O', 'O', 'O', 'O'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.']]
+# grid = [['.', '.', '.', '.', '.', '.'],
+#         ['.', 'O', 'O', '.', '.', '.'],
+#         ['O', 'O', 'O', 'O', '.', '.'],
+#         ['O', 'O', 'O', 'O', 'O', '.'],
+#         ['.', 'O', 'O', 'O', 'O', 'O'],
+#         ['O', 'O', 'O', 'O', 'O', '.'],
+#         ['O', 'O', 'O', 'O', '.', '.'],
+#         ['.', 'O', 'O', '.', '.', '.'],
+#         ['.', '.', '.', '.', '.', '.']]
 
-Think of grid[x][y] as being the character at the x- and y-coordinates of a “picture” drawn with text characters. The (0, 0) origin is 
-in the upper-left corner, the x-coordinates increase going right, and the y-coordinates increase going down.
+# Think of grid[x][y] as being the character at the x- and y-coordinates of a “picture” drawn with text characters. The (0, 0) origin is 
+# in the upper-left corner, the x-coordinates increase going right, and the y-coordinates increase going down.
 
-Copy the previous grid value, and write code that uses it to print the image.
+# Copy the previous grid value, and write code that uses it to print the image.
 
-..OO.OO..
-.OOOOOOO.
-.OOOOOOO.
-..OOOOO..
-...OOO...
-....O....
+# ..OO.OO..
+# .OOOOOOO.
+# .OOOOOOO.
+# ..OOOOO..
+# ...OOO...
+# ....O....
 
-
+########################################################################################################################################
 
 
 grid = [['.', '.', '.', '.', '.', '.'],
