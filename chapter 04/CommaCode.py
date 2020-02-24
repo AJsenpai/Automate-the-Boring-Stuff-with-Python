6175df71d7f317601367ce24266683652d506096
Ajay <ajayidc007@gmail.com>
2020-02-24 14:32:36 +0530

    Create CommaCode.py

---
+++ chapter 04/CommaCode.py
@@ -0,0 +1,34 @@
+# Date: 02/24/2020
+# Chapter: List
+    
+        
+# def sentence_maker(mylist):
+    
+    
+#     sentence=''
+#     for i in range(0,len(mylist)-1):
+#         sentence+=mylist[i]+', '
+#     try:
+#         sentence+='and '+mylist[-1]
+#     except:
+#         print('[]')
+#     else:
+#         if len(mylist)==1:
+#             print(mylist[0])
+#         else:
+#             print(sentence)
+    
+    # another approach
+    # if len(mylist)==1:
+    #     print(mylist[0])
+    # else:
+    #     mylist.insert(len(mylist)-1,'and')
+    #     print(*mylist, sep=', ')
+    
+    
+    
+# spam = ['apples', 'bananas', 'tofu', 'cats' , 'eggs' , 'ketchup']  #testcase1
+# spam = ['apples', 'oranges' , 'eggs']                              #testcase2
+# spam= []                                                           #testcase3
+
+# sentence_maker(spam)
