53f90e605c9255a0a4af665306f0bf3406e20167
Ajay <ajayidc007@gmail.com>
2020-02-24 14:03:37 +0530

    Create Functions.py

---
+++ chapter03/Functions.py
@@ -0,0 +1,19 @@
+# Automate python : Assignments
+# Author : Ajay 
+
+# Date : 02/20/2020
+# Topic: Function
+
+# def collatz(number):
+#     return number//2 if number%2==0 else  3 * (number+1)
+
+# while  True:
+#         try:
+#             takeval= int(input())
+#             if collatz(takeval)!=1:
+#                 print(collatz(takeval))
+#             else:
+#                 print(collatz(takeval))
+#                 break
+#         except:
+#             print("The Value must be an integer")   
