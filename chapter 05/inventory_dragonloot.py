05c0f5214d97252bb37b25ff29516cb18fd94712
Ajay <ajayidc007@gmail.com>
2020-02-27 09:54:54 +0530

    Update inventory_dragonloot.py

--- chapter 05/inventory_dragonloot.py
+++ chapter 05/inventory_dragonloot.py
@@ -1,6 +1,13 @@
 # Date: 2/27/2020
 # Topic: Dictonaries
 
+# Imagine that a vanquished dragon’s loot is represented as a list of strings like this:
+
+# dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+# Write a function named addToInventory(inventory, addedItems), where the inventory parameter is a dictionary representing the player’s 
+# inventory (like in the previous project) and the addedItems parameter is a list like dragonLoot. The addToInventory() function should 
+# return a dictionary that represents the updated inventory. Note that the addedItems list can contain multiples of the same item.
 # #########################################################################################################################################
 
 
