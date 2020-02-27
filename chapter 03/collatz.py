7ab97c06d0a0491f06d6d8c5519f29c4bae04f73
Ajay <ajayidc007@gmail.com>
2020-02-27 11:38:28 +0530

    Update collatz.py

--- chapter 03/collatz.py
+++ chapter 03/collatz.py
@@ -1,9 +1,7 @@
-# Automate python : Assignments
 # Author : Ajay 
 # Date : 02/20/2020
 # Chapter 03: Functions
 
-###########################################################################################################################################
 # The Collatz Sequence
 # Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and 
 # return this value. If number is odd, then collatz() should print and return 3 * number + 1.
@@ -11,6 +9,7 @@
 # the value 1. (Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, you’ll arrive at 1! 
 # Even mathematicians aren’t sure why. Your program is exploring what’s called the Collatz sequence, sometimes called “the simplest 
 # impossible math problem.”)
+# ########################################################################################################################################
 
 def collatz(number):
     return number//2 if number%2==0 else  3 * (number+1)
