ee41886e85c08f5674710c3bfae484984bdaaa92
Ajay <ajayidc007@gmail.com>
2020-03-05 16:44:58 +0530

    Create strongpassword.py

---
+++ chapter 06/strongpassword.py
@@ -0,0 +1,45 @@
+# Date: 03/05/2020
+# Topic : Regular Expression with Python
+#
+Strong Password Detection
+Write a function that uses regular expressions to make sure the password string it is passed is strong. A strong password is defined as 
+one that is at least eight characters long, contains both uppercase and lowercase characters, and has at least one digit. You may need to 
+test the string against multiple regex patterns to validate its strength.
+
+
+import re,getpass
+
+def password_checker(pwd_to_check):
+    
+    uppercheck = re.compile(r'[A-Z]')
+    lowercheck = re.compile(r'[a-z]')
+    digitcheck = re.compile(r'[0-9]')
+    specialchar = re.compile(r'[@#$%^&*+=]')
+    
+    moupper = uppercheck.search(pwd_to_check)
+    molower = lowercheck.search(pwd_to_check)
+    modigit = digitcheck.search(pwd_to_check)
+    mospecial = specialchar.search(pwd_to_check)
+    
+    # passwordRegex = re.compile(r'[A-Za-z0-9@#$%^&+=]{8,}')
+    # if re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', pwd_to_check):
+    if len(pwd_to_check)>7 and mospecial and modigit and molower and moupper:
+        print("your password is Strong")
+    else:
+        print(""" \nPassword should contain:
+                  * Atleast one uppercase letter
+                  * Atleast one lowercase letter
+                  * Atleast one digit
+                  * Atleast one special Character = @#$%^&*=+
+                  """)
+        
+# password = input("Enter your password  ")
+password = getpass.getpass("Enter you password ")
+seepassword= input ("press any key to see your paasword else press Enter?\U0001F441   ")
+
+if seepassword == "":
+    password_checker(password)
+else:
+    print(password)
+    password_checker(password)
+
