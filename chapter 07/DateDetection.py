4478924ed82a7dbcdf634a654f884c0d3217adea
Ajay <ajayidc007@gmail.com>
2020-03-12 13:36:22 +0530

    Update DateDetection.py

--- chapter 07/DateDetection.py
+++ chapter 07/DateDetection.py
@@ -1,6 +1,7 @@
 # Date: 03/12/2020 (lol ironic)
 # Topic: Regular Expressions
 #
+# ### Practice Project ###
 # Write a regular expression that can detect dates in the DD/MM/YYYY format. Assume that the days range from 01 to 31, the months range 
 # from 01 to 12, and the years range from 1000 to 2999. Note that if the day or month is a single digit, it’ll have a leading zero.
 # The regular expression doesn’t have to detect correct days for each month or for leap years; it will accept nonexistent dates like 
