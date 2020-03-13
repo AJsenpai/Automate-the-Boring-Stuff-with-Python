96c778eb8bcdcc2e366d578b6302249544d138d5
Ajay <ajayidc007@gmail.com>
2020-03-13 23:58:12 +0530

    Topic : Regex

--- chapter 07/strongpassword.py
+++ chapter 07/strongpassword.py
@@ -1,7 +1,7 @@
 # Date: 03/05/2020
 # Topic : Regular Expression with Python
 #
-### Practice Project ###
+# ### Practice Project ###
 # Strong Password Detection
 # Write a function that uses regular expressions to make sure the password string it is passed is strong. A strong password is defined as 
 # one that is at least eight characters long, contains both uppercase and lowercase characters, and has at least one digit. You may need to 
