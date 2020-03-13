c6220f06f6060082228cec2af9cf58bd3800bc32
Ajay <ajayidc007@gmail.com>
2020-03-14 00:00:04 +0530

    Random Stuff

--- Misc/tableprinter.py
+++ Misc/tableprinter.py
@@ -1,7 +1,7 @@
 # Date: 03/03/2020
 # Topic: Manipulating Strings 
 
-### Practice Project ###
+# ### Practice Project ###
 # Write a function named printTable() that takes a list of lists of strings and displays it in a well-organized table with each column 
 # right-justified. Assume that all the inner lists will contain the same number of strings. For example, the value could look like this:
 
